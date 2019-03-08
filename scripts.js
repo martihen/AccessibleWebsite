e37b73761c22370d0c6a127998e21536d9beed8e
martihen <34276897+martihen@users.noreply.github.com>
2019-03-08 16:50:32 -0600

    added website main files

---
+++ scripts.js
@@ -0,0 +1,31 @@
+var ctrls = document.createElement('ul');
+
+ctrls.className = 'controls';
+ctrls.innerHTML = '<li>' +
+    '<button type="button" class="btn-prev">' +
+      '<img src="img/chevron-left.png" alt="Previous Item">' +
+    '</button>' +
+  '</li>' +
+  '<li>' +
+    '<button type="button" class="btn-next">' +
+      '<img src="img/chevron-right.png" alt="Next Item">' +
+    '</button>' +
+  '</li>';
+
+ctrls.querySelector('.btn-prev').addEventListener('click', function(){
+  prevSlide(true);
+});
+
+ctrls.querySelector('.btn-next').addEventListener('click', function(){
+  nextSlide(true);
+});
+
+carousel.appendChild(ctrls);
+
+
+
+var liveregion = document.createElement('div');
+liveregion.setAttribute('aria-live', 'polite');
+liveregion.setAttribute('aria-atomic', 'true');
+liveregion.setAttribute('class', 'liveregion visuallyhidden');
+carousel.appendChild(liveregion);
